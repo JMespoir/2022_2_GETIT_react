a1b62c610622f96408070bc10735042ff5c7ea76
JMEspoir <maro3534@knu.ac.kr>
2022-10-12 11:19:59 +0900

    class_6

---
+++ chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+
+function ProfileCard(props){
+    return(
+        <Card title = "Jaemin Park" backgroundColor = "#97CEF7">
+            <p>안녕하세요, 게린이입니다.</p>
+            <p>저는 리액트를 사용해서 개발하고 있습니다.</p>
+        </Card>
+    )
+}
+export default ProfileCard;
